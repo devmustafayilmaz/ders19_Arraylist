cb26dedb9480d3b9a03b2a47eaf74ce73f8be2b7
devmustafayilmaz <73992749+devmustafayilmaz@users.noreply.github.com>
2022-10-22 00:42:12 +0300

    Update C06_FibonacciSorusu.java

--- C06_FibonacciSorusu.java
+++ C06_FibonacciSorusu.java
@@ -13,13 +13,53 @@ public class C06_FibonacciSorusu {
 
         if (girilenSayi<0 ){
             System.out.println("Fibonacci serisi icin 0 'den buyuk tamsayi girmelisin..");
-        }else if (){
+        }
+        }else if (girilenSayi==0){
+            System.out.println("0'dan kucuk fibonacci sayisi yoktur.");
+        }
+
+        }else if (girilenSayi==1){
+            System.out.println("1'den kucuk tek fibonacci sayisi 0'dir.")
+            
+        }else {
+            
+                fibonacciSayilariniyazdir(girilenSayi);
 
         }
 
 
-
-
-
     }
+      public static void fibonacciSayilariniyazdir(int girilenSayi){
+          // buraya geldiyse girilen sayi 1'den buyuktur.
+      
+      List<Integer>fibonacciListesi= new ArrayList<>();
+          fibonacciListesi.add(0);
+          fibonacciListesi.add(1);
+          fibonacciListesi.add(1);
+          
+          int yeniFibonacciSayisi=0;
+          int index=3;// ilk 3 eleman 0 1 1 oldugu icin 3.index e geldik
+          
+          while(yeniFibonacciSayisi<girilenSayi){
+          
+            yeniFibonacciSayisi= fibonacciListesi.get(index-2) + fibonacciListesi.get(index-1);
+              if(yeniFibonacciSayisi<girilenSayi){
+                fibonacciListesi.add(yeniFibonacciSayisi);
+                  
+              }
+              index++;
+          
+          }
+      
+            System.out.println(fibonacciListesi);
+      
+      }
+        
+
+
+
+
+
+
+
 }
